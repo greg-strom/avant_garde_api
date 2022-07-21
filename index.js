bf189462926c9077494e725c6e64bd21941b078f
gbstrom <gbstrom@gmail.com>
2022-07-21 15:22:06 -0400

    set up morgan to log directly to log.txt

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path');
+
 const app = express();
 
-app.use(morgan('common'));
+// this line creates a write stream (in append mode) and a ‘log.txt’ file in root directory
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
+
+// this line sets up the logger that logs requests to log.txt
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/secreturl', (req, res) => {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
