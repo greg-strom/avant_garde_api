a1b4d312b77fbf2efb85354c401d40f63de5926e
gbstrom <gbstrom@gmail.com>
2022-08-15 13:41:16 -0400

    added code to test cors

--- index.js
+++ index.js
@@ -18,6 +18,19 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 // this line creates a write stream (in append mode) and a ‘log.txt’ file in root directory
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' })
 
